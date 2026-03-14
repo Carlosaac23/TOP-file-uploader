ed551e98eb6ce0483c8489b7678be84bcadc5c4d
Carlos <carlosaac232001@gmail.com>
2026-03-13 22:35:33 -0500

    feat: create Folder model and create relations with File and User

---
+++ prisma/migrations/20260314032803_add_folders/migration.sql
@@ -0,0 +1,19 @@
+-- AlterTable
+ALTER TABLE "files" ADD COLUMN     "folderId" TEXT;
+
+-- CreateTable
+CREATE TABLE "folders" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "folders_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "files" ADD CONSTRAINT "files_folderId_fkey" FOREIGN KEY ("folderId") REFERENCES "folders"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "folders" ADD CONSTRAINT "folders_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
