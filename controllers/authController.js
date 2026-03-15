3fd074f8718e0fd8e1ad71d0ab405de63ae9954d
Carlos <carlosaac232001@gmail.com>
2026-03-14 23:31:15 -0500

    chore: make auth routes/controllers only to get user home logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,46 +1,10 @@
-import passport from 'passport';
-
-import { generateHashedPassword } from '../helpers/passwords.js';
 import { prisma } from '../lib/prisma.js';
 
-export function getRegister(req, res) {
-  res.render('register');
-}
+export async function getUserHomeController(req, res) {
+  const user = req.user;
+  console.log('Current user:', req.user);
+  const folders = await prisma.folder.findMany({ where: { userId: user.id } });
+  console.log('Folders:', folders);
 
-export async function postRegister(req, res) {
-  try {
-    console.log(req.body);
-    await prisma.user.create({
-      data: {
-        name: req.body.name,
-        email: req.body.email,
-        password: await generateHashedPassword(req.body.password, 12),
-      },
-    });
-
-    res.redirect('/');
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-export function getLoginController(req, res) {
-  res.render('index');
-}
-
-export const postLoginController = passport.authenticate('local', {
-  failureRedirect: '/',
-  successRedirect: '/success',
-});
-
-export function logoutController(req, res) {
-  req.logout(error => {
-    if (error) return res.status(500).send('Logout failed');
-
-    res.redirect('/');
-  });
-}
-
-export function successController(req, res) {
-  res.render('success');
+  res.render('pages/home', { user, folders });
 }
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,18 +1,7 @@
 import { Router } from 'express';
 
-import {
-  getRegister,
-  postRegister,
-  getLoginController,
-  postLoginController,
-  logoutController,
-  successController,
-} from '../controllers/authController.js';
-import isAuth from '../middleware/auth.js';
+import { getUserHomeController } from '../controllers/authController.js';
 
 export const authRoutes = Router();
 
-authRoutes.route('/').get(getLoginController).post(postLoginController);
-authRoutes.route('/register').get(getRegister).post(postRegister);
-authRoutes.get('/logout', logoutController);
-authRoutes.get('/success', isAuth, successController);
+authRoutes.get('/home', getUserHomeController);
