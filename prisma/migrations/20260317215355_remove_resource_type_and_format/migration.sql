9ac7394874763191cba3442abd4df777974140a4
Carlos <carlosaac232001@gmail.com>
2026-03-17 18:25:00 -0500

    feat: remove some rows from File model

---
+++ prisma/migrations/20260317215355_remove_resource_type_and_format/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `format` on the `files` table. All the data in the column will be lost.
+  - You are about to drop the column `resourceType` on the `files` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "files" DROP COLUMN "format",
+DROP COLUMN "resourceType";
